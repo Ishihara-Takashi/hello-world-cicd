006c1a36a0fab59807232523421e793a16eb4bc4
IshiharaTakashi <ishihara.takashi@ryobi.co.jp>
2024-10-31 11:55:18 +0900

    source修正

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,8 +10,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "{YOUR-S3-BUCKET-NAME}"
-    key     = "{YOUR-S3-BUCKET-KEY}"
+    bucket  = "aws-ecs-terraform-tfstate-ishihara-test"
+    key     = "tfstylog.tfstate"
     region  = "ap-northeast-1"
     profile = "terraform"
   }
